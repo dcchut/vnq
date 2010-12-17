7c3e0894a9f95dca692793333a6f95ccdd78b748
Robert Usher <266585+dcchut@users.noreply.github.com>
2010-12-17 22:54:50 +1100

    work

--- application/views/admin/moderate.php
+++ application/views/admin/moderate.php
@@ -3,10 +3,10 @@
 <?php echo Form::open('admin/moderate/'); ?><br />
 
 <p>qid:</p>
-<?php echo Form::input('qid', FALSE, array('class' => 'qid')); ?><br /><br />
+<?php echo Form::input('qid', FALSE, array('class' => 'qid')); ?><br />
 
 <p>action:</p>
-<?php echo Form::select('action', array('show', 'hide')); ?><br /><br />
+<?php echo Form::select('action', array('show', 'hide')); ?><br />
 
 <?php echo Form::submit('submit', 'do it'); ?>
 <?php echo Form::close(); ?>
