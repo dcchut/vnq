a931d2b6638f33f7318655363817e67ecca882e0
Robert Usher <266585+dcchut@users.noreply.github.com>
2010-12-17 23:13:21 +1100

    work

--- application/views/admin/moderate.php
+++ application/views/admin/moderate.php
@@ -1,15 +1,15 @@
-<b>quote acceptance panel:</b><br />
+<b>quote acceptance panel:</b><br /><br />
 <?php echo $panel; ?>
 <br /><br />
 <b>quote fiddle panel:</b><br />
 <?php echo Form::open('admin/moderate/', array('class', 'hide')); ?>
 <?php if (isset($message) && !empty($message)): ?>
-<p><?php echo $message; ?></p>
+<p class="np"><?php echo $message; ?></p>
 <?php endif; ?>
 <p class="np">qid:</p>
 <?php echo Form::input('id', FALSE, array('class' => 'qid')); ?><br />
 
-<p class="">action:</p>
+<p class="np">action:</p>
 <?php echo Form::select('action', $options); ?><br /><br />
 
 <?php echo Form::submit('submit', 'do it'); ?>
