2ab93215a3974fba73b7798e44ab87a282bb1b33
robbo <dcchut@gmail.com>
2010-12-17 22:39:59 +1100

    work

--- application/views/admin/moderate.php
+++ application/views/admin/moderate.php
@@ -1,6 +1,6 @@
 <br />
-<?php echo Form::open('admin/moderate/'); ?>
-<?php echo Form::select('action', array('show', 'hide')); ?><br /><br />
-<?php echo Form::input('qid'); ?><br />
+<?php echo Form::open('admin/moderate/'); ?><br />
+Quote (ID): <?php echo Form::input('qid'); ?><br />
+Action: <?php echo Form::select('action', array('show', 'hide')); ?><br />
 <?php echo Form::submit('submit', 'Submit'); ?>
 <?php echo Form::close(); ?>
