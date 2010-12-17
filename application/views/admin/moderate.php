456084eb2031644247eea7cab8691bd016a84fab
robbo <dcchut@gmail.com>
2010-12-17 23:13:53 +1100

    work

--- application/views/admin/moderate.php
+++ application/views/admin/moderate.php
@@ -1,13 +1,13 @@
 <b>quote acceptance panel:</b><br /><br />
 <?php echo $panel; ?>
 <br /><br />
-<b>quote fiddle panel:</b><br />
+<b>quote fiddle panel:</b><br /><br />
 <?php echo Form::open('admin/moderate/', array('class', 'hide')); ?>
 <?php if (isset($message) && !empty($message)): ?>
 <p class="np"><?php echo $message; ?></p>
 <?php endif; ?>
 <p class="np">qid:</p>
-<?php echo Form::input('id', FALSE, array('class' => 'qid')); ?><br />
+<?php echo Form::input('id', FALSE, array('class' => 'qid')); ?><br /><br />
 
 <p class="np">action:</p>
 <?php echo Form::select('action', $options); ?><br /><br />
