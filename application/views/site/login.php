55fd270c076e070d3cc43544a6a5d75792a0135c
Robert Usher <266585+dcchut@users.noreply.github.com>
2010-12-24 00:08:14 +1100

    changes

--- application/views/site/login.php
+++ application/views/site/login.php
@@ -2,5 +2,5 @@
 username: <?php echo Form::input('username') ?><br />
 password: <?php echo Form::password('password') ?><br />
 <br />
-<?php echo Form::submit('submit', 'Login'); ?>
+<?php echo Form::submit('submit', 'login'); ?>
 <?php echo Form::close(); ?>
