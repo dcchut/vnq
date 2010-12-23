09a723df0e4a79939c3b9441bdc01352be100120
robbo <dcchut@gmail.com>
2010-12-24 00:12:21 +1100

    heaps of changes

--- application/views/quotes/submit.php
+++ application/views/quotes/submit.php
@@ -1,4 +1,4 @@
-please enter your quote in the field below.  quotes must be approved by a moderator before they are publically visible.  please remove any timestamps and unnecessary text.
+please enter your quote in the field below, ensuring that you remove any timestamps and unnecessary text.  quotes must be approved by a moderator before they are publically visible.
 <br /><br />
 <?php echo Form::open('quotes/submit2/'); ?>
 <?php echo Form::textarea('quote'); ?><br /><br />
