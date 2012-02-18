dd86d012908a1c8d97cfd41bb817b73849e83894
Robert Usher <266585+dcchut@users.noreply.github.com>
2012-02-18 19:37:42 +1100

    Reverted ugly changes

--- application/views/site/template.php
+++ application/views/site/template.php
@@ -2,7 +2,7 @@
         "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="en-US">
     <head profile="http://gmpg.org/xfn/11">
-      <title><?php echo $title ?> - where dreams go to die</title>
+      <title><?php echo $title ?></title>
       <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
       <link href='http://fonts.googleapis.com/css?family=Kameron:400,700|Inconsolata&v1' rel='stylesheet' type='text/css'>
       <?php echo HTML::script('media/jquery.js') ?>
