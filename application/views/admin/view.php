c250f059e4250346bc9fe46be8ee7b7e9931e22f
Robert Usher <266585+dcchut@users.noreply.github.com>
2012-06-17 03:05:28 +1000

    parse error fix

--- application/views/admin/view.php
+++ application/views/admin/view.php
@@ -12,8 +12,8 @@
             <span class="bcol acol_id"><?php echo $quote->id; ?></span>
             <span class="bcol acol_date"><?php echo date("H:i:s d/m/Y", $quote->date); ?></span>
             <span class="bcol acol_up"><?php echo $quote->up; ?></span>
-            <span class="bcol acol_status"><?php if ($quote->status == 1) echo 'accepted' 
-                                             elseif ($quote->status == 3) echo 'rejected'
+            <span class="bcol acol_status"><?php if ($quote->status == 1) echo 'accepted';
+                                             elseif ($quote->status == 3) echo 'rejected';
                                              else                         echo 'unmoderated'; ?></span>
             <span class="bcol acol_ip lcol"><?php echo $quote->ip; ?></span>
             <div class="aclr"></div>
