c36d0cab2a0671bcf734ee99781ec2e8b8d17a63
Robert Usher <266585+dcchut@users.noreply.github.com>
2012-06-17 02:59:24 +1000

    intriguing visual bug

--- application/views/admin/view.php
+++ application/views/admin/view.php
@@ -2,7 +2,6 @@
 <div class="arow">
     <span class="acol acol_id">id</span>
     <span class="acol acol_date">date</span>
-    <span class="acol acol_quote">quote</span>
     <span class="acol acol_up">up</span>
     <span class="acol acol_status">status</span>
     <span class="acol acol_ip">ip</span>
@@ -12,7 +11,6 @@
         <div class="arow">
             <span class="bcol acol_id"><?php echo $quote->id; ?></span>
             <span class="bcol acol_date"><?php echo $quote->date; ?></span>
-            <span class="bcol acol_quote"><?php echo htmlentities(substr($quote->quote,0,20)); ?></span>
             <span class="bcol acol_up"><?php echo $quote->up; ?></span>
             <span class="bcol acol_status"><?php echo $quote->status; ?></span>
             <span class="bcol acol_ip"><?php echo $quote->ip; ?></span>
