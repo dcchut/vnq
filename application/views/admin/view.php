26a80ccc1a40d9367ab9f8aeab726c0808234870
Robert Usher <266585+dcchut@users.noreply.github.com>
2012-06-17 03:02:25 +1000

    added date formatting

--- application/views/admin/view.php
+++ application/views/admin/view.php
@@ -10,10 +10,10 @@
     <?php foreach($quotes as $quote): ?>
         <div class="arow">
             <span class="bcol acol_id"><?php echo $quote->id; ?></span>
-            <span class="bcol acol_date"><?php echo $quote->date; ?></span>
+            <span class="bcol acol_date"><?php echo date("H:i:s d/m/Y", $quote->date); ?></span>
             <span class="bcol acol_up"><?php echo $quote->up; ?></span>
             <span class="bcol acol_status"><?php echo $quote->status; ?></span>
-            <span class="bcol acol_ip"><?php echo $quote->ip; ?></span>
+            <span class="bcol acol_ip lcol"><?php echo $quote->ip; ?></span>
             <div class="aclr"></div>
         </div>
     <?php endforeach; ?>
