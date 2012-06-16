620dd0849479a584c0654ccfd26780cd2baa54d5
robbo <dcchut@gmail.com>
2012-06-17 02:49:31 +1000

    visual changes

--- application/views/admin/view.php
+++ application/views/admin/view.php
@@ -7,12 +7,14 @@
     <span class="acol acol_ip">ip</span>
     <br />
     <?php foreach($quotes as $quote): ?>
-        <span class="acol acol_id"><?php echo $quote->id; ?></span>
-        <span class="acol acol_date"><?php echo $quote->date; ?></span>
-        <span class="acol acol_quote"><?php echo htmlentities($quote->quote); ?></span>
-        <span class="acol acol_up"><?php echo $quote->up; ?></span>
-        <span class="acol acol_status"><?php echo $quote->status; ?></span>
-        <span class="acol acol_ip"><?php echo $quote->ip; ?></span>
+        <div class="quote">
+            <span class="bcol acol_id"><?php echo $quote->id; ?></span>
+            <span class="bcol acol_date"><?php echo $quote->date; ?></span>
+            <span class="bcol acol_quote"><?php echo htmlentities(substr($quote->quote,0,20)); ?></span>
+            <span class="bcol acol_up"><?php echo $quote->up; ?></span>
+            <span class="bcol acol_status"><?php echo $quote->status; ?></span>
+            <span class="bcol acol_ip"><?php echo $quote->ip; ?></span>
+        </div>
         <br />
     <?php endforeach; ?>
 </div>
