40db0cd84a9a60fc836de6c105256691688d5da8
dcchut <dcchut@gmail.com>
2012-06-17 14:56:21 +1000

    changed visual styles

--- application/views/admin/view.php
+++ application/views/admin/view.php
@@ -9,13 +9,13 @@
 </div>
     <?php foreach($quotes as $quote): ?>
         <div class="arow">
-            <span class="bcol acol_id"><?php echo HTML::anchor($quote->id, '#' . $quote->id); ?></span>
-            <span class="bcol acol_date"><?php echo date("H:i:s, d/m/Y", $quote->date); ?></span>
-            <span class="bcol acol_up"><?php echo $quote->up; ?></span>
-            <span class="bcol acol_status"><?php if ($quote->status == 1) echo 'accepted';
+            <span class="acol acol_id"><?php echo HTML::anchor($quote->id, '#' . $quote->id); ?></span>
+            <span class="acol acol_date"><?php echo date("H:i:s, d/m/Y", $quote->date); ?></span>
+            <span class="acol acol_up"><?php echo $quote->up; ?></span>
+            <span class="acol acol_status"><?php if ($quote->status == 1) echo 'accepted';
                                              elseif ($quote->status == 3) echo 'rejected';
                                              else                         echo 'unmoderated'; ?></span>
-            <span class="bcol acol_ip lcol"><?php echo $quote->ip; ?></span>
+            <span class="acol acol_ip lcol"><?php echo $quote->ip; ?></span>
             <div class="aclr"></div>
         </div>
     <?php endforeach; ?>
